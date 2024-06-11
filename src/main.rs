38ac381cf8d60a2557a91d308aa88ac71361dd73
Ryan C <ryanc@lastyard.com>
2024-06-11 18:15:40 +0800

    Add "Block" style elements to markdown

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-use clap::{App, Arg, ArgMatches, SubCommand};
+use clap::{Arg, ArgMatches, Command};
 use mdbook::book::{Book, Chapter};
 use mdbook::errors::Error;
 use mdbook::preprocess::{CmdPreprocessor, Preprocessor, PreprocessorContext};
+use mdbook::utils::new_cmark_parser;
 use pulldown_cmark::{CowStr, Event, Parser, Tag};
 use std::io;
 use std::process;
+use std::collections::VecDeque;
+
+const MAX_DEPTH: usize = 254;
 
 #[derive(Default)]
-pub struct Classy;
+pub struct Blocky;
 
-impl Classy {
-    pub fn new() -> Classy {
-        Classy
+impl Blocky{
+    pub fn new() -> Blocky {
+        Blocky
     }
 }
 
-impl Preprocessor for Classy {
+impl Preprocessor for Blocky {
     fn name(&self) -> &str {
-        "classy"
+        "blocky"
     }
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
         book.for_each_mut(|book| {
             if let mdbook::BookItem::Chapter(chapter) = book {
-                if let Err(e) = classy(chapter) {
-                    eprintln!("classy error: {:?}", e);
+                if let Err(e) = blocky(chapter) {
+                    eprintln!("blocky error: {:?}", e);
                 }
             }
         });
@@ -34,91 +38,70 @@ impl Preprocessor for Classy {
     }
 }
 
-#[derive(Debug)]
-struct ClassAnnotation {
-    pub class: String,
-    pub index: usize,
-    pub paragraph_start: usize,
-    pub paragraph_end: Option<usize>,
+struct EventClassAnnotator<'a> {
+    stack: VecDeque<Event<'a>>,
+    depth: usize,
+}
+
+impl<'a> Iterator for EventClassAnnotator<'a> {
+    type Item = Event<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current = self.stack.pop_front()?;
+        if let Event::Text(CowStr::Borrowed(text)) = current {
+            let text_len = text.len();
+            if text_len < 5 {
+                return Some(current)
+            }
+            // If the last event was the opening of a text element
+            if text.starts_with("{:.") && text.ends_with("}") {
+                if self.depth > 253 {
+                    panic!("Error with recursion depth!: {}", text);
+                }
+                self.depth += 1;
+                let mut class = text[3..text_len - 1].to_string();
+                class.push_str(" blocky-block");
+                if self.depth > 1 {
+                    class.push_str(&format!(" block-level-{}", self.depth - 1));
+                }
+                let open_div = Event::Html(CowStr::from(format!("<div class=\"{}\">", class)));
+                return Some(open_div)
+            } else if text.starts_with("{:/.") && text.ends_with("}") {
+                let close_div = Event::Html(CowStr::from("</div>"));
+                if self.depth == 0 {
+                    // Bad formatting
+                    panic!("Bad formatting!: {}", text);
+                }
+                self.depth -= 1;
+                return Some(close_div)
+            } else {
+                Some(current)
+            }
+        } else {
+            Some(current)
+        }
+    }
+
+}
+
+impl<'a> EventClassAnnotator<'a> {
+    fn new(stack: Vec<Event<'a>>) -> Self {
+        Self {
+            stack: stack.into(),
+            depth: 0
+        }
+    }
 }
 
 /// This is where the markdown transformation actually happens.
 /// Take paragraphs beginning with `{:.class-name}` and give them special rendering.
 /// Mutation: the payload here is that it edits chapter.content.
-fn classy(chapter: &mut Chapter) -> Result<(), Error> {
-    // 1. Parse the inbound markdown into an Event vector.
-    let incoming_events: Vec<Event> = Parser::new(&chapter.content).collect();
+fn blocky(chapter: &mut Chapter) -> Result<(), Error> {
+    let incoming_events: Vec<Event> = new_cmark_parser(&chapter.content, false).collect();
+    let new_events: Vec<Event> = EventClassAnnotator::new(incoming_events).collect();
 
-    // 2. Find paragraphs beginning with the class annotator `{:.class-name}` and record their information in
-    // a vector of ClassAnnotation structs.
-    let mut class_annotations: Vec<ClassAnnotation> = vec![];
-    for i in 0..incoming_events.len() {
-        let event = &incoming_events[i];
-        match *event {
-            Event::Text(CowStr::Borrowed(text)) => {
-                if i > 0 {
-                    if let Event::Start(Tag::Paragraph) = incoming_events[i - 1] {
-                        let v: Vec<_> = text.split("").collect();
-                        let len_v = v.len();
-                        if v[..4].join("") == "{:." && v[(len_v - 2)..].join("") == "}" {
-                            let class = v[4..(len_v - 2)].join("");
-                            class_annotations.push(ClassAnnotation {
-                                class,
-                                index: i,
-                                paragraph_start: i - 1,
-                                paragraph_end: None,
-                            })
-                        }
-                    }
-                }
-            }
-            Event::End(Tag::Paragraph) => {
-                let last = class_annotations.last_mut();
-                if let Some(class_command) = last {
-                    if class_command.paragraph_end.is_none() {
-                        class_command.paragraph_end = Some(i);
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
-
-    // 3. Construct a new_events vector with <div class="class-name">\n \n</div> around the annotated paragraphs
-    // (and with the class annotation removed).
-    let mut slices = vec![];
-    let mut last_end = 0;
-    let div_starts: Vec<Event> = class_annotations
-        .iter()
-        .map(|ca| Event::Html(CowStr::from(format!("<div class=\"{}\">", ca.class))))
-        .collect();
-    let div_end: Vec<Event> = vec![Event::Html(CowStr::from("</div>"))];
-    for (i, ca) in class_annotations.iter().enumerate() {
-        // Add unclassed events.
-        slices.push(&incoming_events[last_end..ca.paragraph_start]);
-
-        last_end = ca.paragraph_end.unwrap_or(incoming_events.len() - 1);
-
-        let paragraph = &incoming_events[ca.paragraph_start..(last_end + 1)];
-
-        // Add <div class="class-name">
-        slices.push(&div_starts[i..i + 1]);
-
-        // Add paragraph opener.
-        slices.push(&paragraph[0..1]);
-
-        // Add the rest of the paragraph, skipping the class annotation.
-        slices.push(&paragraph[2..]);
-
-        // Add </div>.
-        slices.push(&div_end[..]);
-    }
-    slices.push(&incoming_events[last_end..]);
-    let new_events = slices.concat();
-
-    // 4. Update chapter.content using markdown generated from the new event vector.
     let mut buf = String::with_capacity(chapter.content.len() + 128);
-    pulldown_cmark_to_cmark::cmark(new_events.into_iter(), &mut buf, None)
+    pulldown_cmark_to_cmark::cmark(new_events.into_iter(), &mut buf)
         .expect("can re-render cmark");
     chapter.content = buf;
     Ok(())
@@ -148,9 +131,9 @@ fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
     Ok(())
 }
 
-/// Check to see if we support the processor (classy only supports html right now)
+/// Check to see if we support the processor (blocky only supports html right now)
 fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
-    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let renderer = sub_args.get_one::<String>("renderer").expect("Required argument");
     let supported = pre.supports_renderer(&renderer);
 
     if supported {
@@ -162,17 +145,17 @@ fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
 
 fn main() {
     // 1. Define command interface, requiring renderer to be specified.
-    let matches = App::new("classy")
-        .about("A mdbook preprocessor that recognizes kramdown style paragraph class annotation.")
+    let matches = Command::new("blocky")
+        .about("A mdbook preprocessor that lets you design block sections for collections of elements")
         .subcommand(
-            SubCommand::with_name("supports")
-                .arg(Arg::with_name("renderer").required(true))
+            Command::new("supports")
+                .arg(Arg::new("renderer").required(true))
                 .about("Check whether a renderer is supported by this preprocessor"),
         )
         .get_matches();
 
     // 2. Instantiate the preprocessor.
-    let preprocessor = Classy::new();
+    let preprocessor = Blocky::new();
 
     if let Some(sub_args) = matches.subcommand_matches("supports") {
         handle_supports(&preprocessor, sub_args);
